9c32fb86d012aa87ce93474b5e7ace026003cb0c
Karena <iv23220@bristol.ac.uk>
2025-03-07 16:28:02 +0000

    created cards inside collapse

--- everyfamily-frontend/src/components/Dashboard/index.jsx
+++ everyfamily-frontend/src/components/Dashboard/index.jsx
@@ -1,13 +1,59 @@
 import ResourceCard from "./resourceCard.jsx";
+// import "./index.css";
+import React from 'react';
+import {Row, Flex, Collapse } from 'antd';
 
 function Dashboard() {
-  const allResources = ["a", "b", "c"];
+  const allResources = [
+      {title: "A", description: "a"}, {title: "B", description: "b"},
+      {title: "C", description: "c"}, {title: "D", description: "d"},
+      {title: "E", description: "e"}, {title: "F", description: "f"}];
+    const items = [
+        {
+            key: '1',
+            label: 'All resources',
+            children:
+                <Row gutter={16}>
+                    <Flex>
+                        {allResources.map(({title,description}, index) => (
+                            <ResourceCard key={index} title={title} description={description} />
+                        ))}
+                    </Flex>
+                </Row>,
+        },
+        {
+            key: '2',
+            label: 'Arrest',
+            children:
+                <Row gutter={16}>
+                    <Flex>
+                        {allResources.map(({title,description}, index) => (
+                            <ResourceCard key={index} title={title} description={description} />
+                        ))}
+                    </Flex>
+                </Row>,
+            showArrow: false,
+        },
+        {
+            key: '3',
+            label: 'Court',
+            children:<p></p>,
+        },
+        {
+            key: '4',
+            label: 'Imprisonment',
+            children:<p></p>,
+        }
+    ];
+    const onChange = (key) => {
+        console.log(key);
+    };
   return (
-    <main className="resourceCards">
-      {allResources.map((title, index) => (
-        <ResourceCard key={index} title={title} />
-      ))}
-    </main>
+    <div className="dashboard">
+      <main className="browse_by_category">
+          <Collapse items={items} defaultActiveKey={['1']} bordered={false} onChange={onChange} gost/>
+      </main>
+    </div>
   );
 }
 
--- everyfamily-frontend/src/components/Dashboard/resourceCard.jsx
+++ everyfamily-frontend/src/components/Dashboard/resourceCard.jsx
@@ -1,21 +1,44 @@
 import "./ResourceCard.css";
-import { Card } from "antd";
+import { Card, Col } from "antd";
+const { Meta } = Card;
 
-function ResourceCard({ title }) {
+function ResourceCard({ title, description}) {
   return (
-    <Card
-      hoverable
-      style={{
-        width: 240,
-      }}
-      title={title}
-      cover={
-        <img
-          alt="example"
-          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-        />
-      }
-    ></Card>
+    <div className="resource-card">
+        <Col span={8}>
+            <Card
+                hoverable
+                className="card-container"
+                size="small"
+                style={{
+                    width: 200,
+                    height: 200,
+                }}
+                cover={
+                    <img
+                        alt="example"
+                        src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                        style={{
+                            width: '100%',
+                            height: 120,
+                            objectFit: 'cover',}}
+                    />
+                }
+            >
+                <Meta
+                    title={title}
+                    description={description}
+                    style={{
+                        height: 80
+                    }}
+                />
+                {/*<p className='card_title'>{title}</p>*/}
+                {/*<p className='card_desciption'>{description}</p>*/}
+            </Card>
+        </Col>
+
+
+    </div>
   );
 }
 
