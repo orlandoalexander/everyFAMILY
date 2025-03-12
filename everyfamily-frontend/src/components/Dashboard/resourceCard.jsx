19a55f23aa70bfcc575b6902e160ac66aa1a787d
Karena <iv23220@bristol.ac.uk>
2025-03-12 20:09:58 +0000

    image->inner card, scrollable card content

--- everyfamily-frontend/src/components/Dashboard/resourceCard.jsx
+++ everyfamily-frontend/src/components/Dashboard/resourceCard.jsx
@@ -1,37 +1,59 @@
-import "./ResourceCard.css";
-import { Card, Col } from "antd";
+import "./resourceCard.css";
+import { Card, Col, Avatar } from "antd";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
 const { Meta } = Card;
 
 function ResourceCard({ title, description }) {
+  const actions = [
+      <EditOutlined key="edit" />,
+      <SettingOutlined key="setting" />,
+      <EllipsisOutlined key="ellipsis" />,
+  ];
   return (
     <div>
       <Col span={8}>
         <Card
-          hoverable
-          size="small"
-          style={{
-            width: 200,
-            height: 200,
-          }}
-          cover={
-            <img
-              alt="example"
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-              style={{
-                width: "100%",
-                height: 120,
-                objectFit: "cover",
-              }}
-            />
-          }
-        >
-          <Meta
-            title={title}
-            description={description}
+            className="outer--card"
+            hoverable
+            size="small"
+            actions={actions}
             style={{
-              height: 80,
+                width: 200,
+                height: 200,
             }}
-          />
+        >
+            <div
+                className="outer--card--div"
+            >
+                <Card
+                    className="inner--card"
+                    type="inner"
+                    style={{
+                        width: 175,
+                        height: 100,
+                    }}
+                    cover={
+                        <img
+                            className="inner--card--img"
+                            alt="example"
+                            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                        />
+                    }
+                />
+                <p style={{
+                    fontSize: 16,
+                }}>
+                    {title}
+                </p>
+                <p style={{
+                    fontSize: 14,
+                }}>
+                    {description}
+                </p>
+            </div>
+
+
         </Card>
       </Col>
     </div>
