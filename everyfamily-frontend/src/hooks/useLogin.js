ad3407dd5db9b4f60b862f479c36eba48d73305b
OFlowerdew <flowerdewoliver@gmail.com>
2025-03-17 15:20:29 +0000

    add useLogin hook for user authentication with API integration

---
+++ everyfamily-frontend/src/hooks/useLogin.js
@@ -0,0 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
+import api from "./api";
+import { useContext } from "react";
+import { AuthContext } from "../AuthContext";
+
+const useLogin = () => {
+    const { login } = useContext(AuthContext);
+
+    const loginUser = async (data) => {
+        const { email, password, remember = false } = data;
+
+        const response = await api.post("/login", { email, password });
+
+        return {
+            ...response.data,
+            remember
+        };
+    };
+
+    return useMutation({
+        mutationFn: loginUser,
+        onSuccess: ({ id, role, remember }) => {
+            login({ role, id, remember });
+        }
+    });
+};
+
+export default useLogin;
