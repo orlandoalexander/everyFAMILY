5e03d2a46a1c84f9b66eefc96a08e0e85fe6fa7f
orlandoalexander <itsorlando@outlook.com>
2025-04-06 01:52:47 +0100

    ready for deploy

--- everyfamily-frontend/src/main.jsx
+++ everyfamily-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
+import "./main.css";
 import App from "./App.jsx";
 import AuthProvider from "./AuthProvider.jsx";
 import { ConfigProvider } from "antd";
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("app")).render(
   <StrictMode>
     <ConfigProvider
       theme={{
